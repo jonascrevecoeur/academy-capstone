f3ed9eaa0e4d9a2cbacdf7bab52bb7495b361942
jonas crevecoeur <jonas.crevecoeur@hotmail.com>
2022-09-08 12:03:33 +0000

    update gitpod url

--- app/src/transform_data_pyspark.py
+++ app/src/transform_data_pyspark.py
@@ -1,1 +1,19 @@
-1
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
+
+spark = SparkSession.builder.getOrCreate()
+
+# The verbose way
+fields = [
+    StructField("name", StringType(), nullable=True),
+    StructField("age", IntegerType(), nullable=True),
+]
+users = spark.createDataFrame(
+    data=[
+        ("Wim", 1),
+        (None, 2),
+    ],
+    schema=StructType(fields),
+)
+
+users.show()
