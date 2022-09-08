4dea5a0482679cda5cad2df2993d7fe17b9be58e
jonas crevecoeur <jonas.crevecoeur@hotmail.com>
2022-09-08 13:02:55 +0000

    Data loaded from aws successfully

---
+++ app/src/load_data_from_aws.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
+from pathlib import Path
+
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.aws/credentials"))
+access_id = config.get('default', "aws_access_key_id") 
+access_key = config.get('default', "aws_secret_access_key")
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:3.0.0 pyspark-shell"
+
+spark = SparkSession.builder.getOrCreate()
+
+sc = spark.sparkContext
+hadoop_conf = sc._jsc.hadoopConfiguration()
+hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3aFileSystem")
+hadoop_conf.set("fs.s3a.access.key", access_id)
+hadoop_conf.set("fs.s3a.secret.key", access_key)
+hadoop_conf.set("fs.s3a.endpoint", "s3.amazonaws.com")
+hadoop_conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.profile.ProfileCredentialsProvider")
+
+df=spark.read.json("s3a://dataminded-academy-capstone-resources/raw/open_aq/data_part_1.json")
+df.show(n = 5)
+
+df.repartition(3).write.json(
+    str("resources" + "/" + "demo_data"), mode="overwrite"
+)
+
+#print(access_id)
